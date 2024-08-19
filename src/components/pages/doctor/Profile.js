68f0cf24824590e82ff32222c50e281a3d0159bc
Wasif <wk5599426@gmail.com>
2024-08-19 15:49:22 +0500

    edited

--- src/components/pages/doctor/Profile.js
+++ src/components/pages/doctor/Profile.js
@@ -43,9 +43,9 @@ function Profile() {
         {/* Navbar */}
         <Navbar />
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-          <h1>
-            Profile page
-          </h1>
+          {doctor && (
+            
+          )}
         </main>
       </div>
     </div>
